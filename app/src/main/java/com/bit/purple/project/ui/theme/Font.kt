a8a5dca299c2107cde6407ff0e1b3b4c1a481d54
toluwagbemiga <tophepzz14@gmail.com>
2025-11-13 12:22:54 +0100

    feat: added custom font

--- app/src/main/java/com/bit/purple/project/ui/theme/Font.kt
+++ app/src/main/java/com/bit/purple/project/ui/theme/Font.kt
@@ -1,2 +1,16 @@
 package com.bit.purple.project.ui.theme
 
+import androidx.compose.ui.text.ExperimentalTextApi // <-- ADD THIS IMPORT
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+// REMOVED: import androidx.compose.ui.text.font.Font.VariationSettings
+import com.bit.purple.project.R
+
+val JostVariable = FontFamily(
+    Font(
+        resId = R.font.jost_variablefont_wght,
+        weight = FontWeight.Normal
+
+    )
+)
